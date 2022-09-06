9c74569d880c33dfcbe10221df9d079e5d908566
Mustafa Cetinkaya <macetinkaya.03@gmail.com>
2022-09-06 18:14:19 +0300

    new practices are done

--- JavaPractice/src/day02/ShapesWithStar.java
+++ JavaPractice/src/day02/ShapesWithStar.java
@@ -1,2 +1,47 @@
-package day02;public class ShapesWithStar {
+package day02;
+
+public class ShapesWithStar {
+
+    public static void main(String[] args) {
+        System.out.println("\t* * * * * * * *");
+        System.out.println("\t* * * * * * * *");
+        System.out.println("\t* * * * * * * *");
+        System.out.println("\t* * * * * * * *");
+        System.out.println("\t* * * * * * * *");
+        System.out.println(                        );
+        System.out.println("\t* * * * * *");
+        System.out.println("\t*            *");
+        System.out.println("\t*            *");
+        System.out.println("\t*            *");
+        System.out.println("\t*            *");
+        System.out.println("\t*            *");
+        System.out.println("\t* * * * * *");
+        System.out.println();
+        System.out.println("\t\t\t     ^");
+        System.out.println("\t\t\t    / \\");
+        System.out.println("\t\t\t   / * \\ ");
+        System.out.println("\t\t\t  / * * \\");
+        System.out.println("\t\t\t / * * * \\");
+        System.out.println("\t\t\t ---------");
+        System.out.println("\t\t\t \\ * * * /");
+        System.out.println("\t\t\t  \\ * * /");
+        System.out.println("\t\t\t   \\ * /");
+        System.out.println("\t\t\t    \\ /");
+        System.out.println("\t\t\t     V");
+        System.out.println();
+        System.out.println("\t\t\t     ^");
+        System.out.println("\t\t\t    / \\");
+        System.out.println("\t\t\t   /   \\ ");
+        System.out.println("\t\t\t  /     \\");
+        System.out.println("\t\t\t /       \\");
+        System.out.println("\t\t\t ---------");
+        System.out.println("\t\t\t \\       /");
+        System.out.println("\t\t\t  \\     /");
+        System.out.println("\t\t\t   \\   /");
+        System.out.println("\t\t\t    \\ /");
+        System.out.println("\t\t\t     V");
+
+
+
+    }
 }
